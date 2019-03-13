33fcd05b9fe7cda652aa74374cde01d5e6f8ad79
郭金瑞(18040607) <18040607@cnsuning.com>
2019-03-13 16:36:19 +0800

    Swift1创建

---
+++ test-swift1/test-swift1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-swift1
+//
+//  Created by 郭金瑞 on 2019/3/13.
+//  Copyright © 2019年 郭金瑞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
